f53f571715d63a7e244ab79bf65d0e3609891888
Dominik Kondrot <dominik.kondrot@gmail.com>
2021-03-12 00:49:00 +0100

    Base Application

---
+++ src/main/java/com/cryptoloan/CryptoloanApplication.java
@@ -0,0 +1,13 @@
+package com.cryptoloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoloanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoloanApplication.class, args);
+    }
+
+}
